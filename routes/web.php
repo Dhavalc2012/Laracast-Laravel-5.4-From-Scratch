4b1bc664930f1122326fe8388c347aed09c8f13f
Dhaval <dhavalchheda@ymail.com>
2018-05-05 14:04:13 +0530

    Laracast Laravel 5.4 from Scratch Chapter 08 Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,8 @@
 <?php
 
-use App\Task;
 
-Route::get('/tasks', function () {
-   $tasks = Task::all();
-    return view('tasks.index',compact('tasks'));
-});
+Route::get('/tasks','TaskController@index');
 
-Route::get('/tasks/{task}', function ($id) {
+Route::get('/tasks/{task}','TaskController@show');
 
-    $task =Task::find($id);
-    return view('tasks.show',compact('task'));
-});
-
-Route::get('about', function () {
-    return view('about');
-});
 
