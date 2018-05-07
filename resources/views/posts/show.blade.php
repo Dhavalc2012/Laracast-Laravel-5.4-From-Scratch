4986b50d384f15d1a79ec95a54faab755f1a83f1
Dhaval <dhavalchheda@ymail.com>
2018-05-07 15:56:32 +0530

    Laracast Laravel 5.4 from scratch Chapter 15 Eloquent Relationships and Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,18 @@
     <div class="col-sm-10">
     <h1 class="col-sm-12">{{$post->title}}</h1>
     <div class="col-sm-12">{{$post->body}}</div>
+    <hr>
+    <div class="comments col-sm-12">
+        <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+<strong>{{ $comment->created_at->diffForHumans() }}:</strong> &nbsp;
+                    {{$comment->body}}
+                </li>
+                @endforeach
+
+        </ul>
+    </div>
     </div>
 
     @endsection
