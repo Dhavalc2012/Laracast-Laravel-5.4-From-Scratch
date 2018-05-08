9a5ab0e3b7b7b58998134b1cf203f84d772299fa
Dhaval <dhavalchheda@ymail.com>
2018-05-08 14:58:24 +0530

    Laracast Laravel 5.4 from scratch Chapter 20 Archives

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Carbon\Carbon;
 
 class PostController extends Controller
 {
@@ -12,9 +13,25 @@ class PostController extends Controller
         $this->middleware('auth')->except(['index','show']);
     }
     public function index(){
-        $posts = Post::latest()->get();
+        $posts = Post::latest()
+            ->filter(request(['month','year']))
+            ->get();
 
-        return view('posts.index',compact('posts'));
+
+ /*       $posts = Post::latest();
+if($month = request('month')){
+        $posts->whereMonth('created_at',Carbon::parse($month)->month);
+}
+
+        if($year = request('year')){
+            $posts->whereYear('created_at',$year);
+        }
+        $posts = $posts->get();*/
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month,count(*) published')
+            ->groupBy('year','month')
+            ->orderByRaw('min(created_at) desc')->get()->toArray();
+
+        return view('posts.index',compact('posts','archives'));
     }
     public function show(Post $post){
         return view('posts.show',compact('post'));
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,7 @@
 <div class="col-sm-2">
-    Sidebar
+    @foreach($archives as $stats)
+        <li>
+            <a href="/?month={{$stats['month']}}&year={{$stats['year']}}">{{$stats['month'] .' ' . $stats['year']}}</a>
+        </li>
+        @endforeach
 </div>
